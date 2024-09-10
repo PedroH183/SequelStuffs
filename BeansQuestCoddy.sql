12a3c98ec92a1676ffc7e18fe86d1d2815d63c48
PedroH183 <pedro_freires18@outlook.com>
2024-09-10 09:43:24 -0300

    feat: beans question added 🛞

--- BeansQuestCoddy.sql
+++ BeansQuestCoddy.sql
@@ -4,18 +4,15 @@ SELECT
             SUM(
                 (density - (SELECT AVG(density) FROM beans)) * (density - (SELECT AVG(density) FROM beans))
             ) / COUNT(*)
-        ), 2
+        ), 3
     ) as STD 
 FROM (
     -- relevant beans 
     SELECT * FROM beans 
     WHERE 
-        diameter_wide > (SELECT AVG(diameter_wide) FROM beans) and
+        (diameter_wide > (SELECT AVG(diameter_wide) FROM beans)) and
         (
-            (shade == 'light' and ((density / diameter_wide) > 0.1)) or 
-            shade == 'dark' or 
-            shade != 'semi-dark'
+            (shade = 'light' and ((density / diameter_wide) > 0.1)) or 
+            shade = 'dark' or shade != 'semi-dark'
         )
-)
-GROUP BY brand
-ORDER BY STD ASC;
+) GROUP BY brand HAVING STD < 0.1 ORDER BY STD ASC;
